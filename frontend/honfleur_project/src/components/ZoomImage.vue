a2b689f2710b220eb7722f8444ad6cabde0b291d
Fabien R <79097567+Zheyn@users.noreply.github.com>
2021-10-06 18:01:37 +0200

    ZoomImage 95%

--- frontend/honfleur_project/src/components/ZoomImage.vue
+++ frontend/honfleur_project/src/components/ZoomImage.vue
@@ -1,31 +1,56 @@
 <template>
   <div
     class="zoom display-zoom"
-    @click="zoomOut"
     @scroll.prevent
     @wheel.prevent
     @touchmove.prevent
   >
-    <img src="../assets/ `{this.image}` .jpg" alt="" />
+    <div class="container-img" @click="zoomOut">
+      <img :src="require('../assets/' + getImages[this.count])" alt="" />
+      <i class="fas fa-times"></i>
+    </div>
     <div class="chevron">
-      <i class="fas fa-chevron-right"></i>
+      <i class="fas fa-chevron-right" @click="chevronNext"></i>
+      <i class="fas fa-chevron-left" @click="chevronPrevious"></i>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   data() {
     return {
-        image: 1
+      count: 0,
     };
   },
   methods: {
     zoomOut() {
       let zoomAttribute = document.querySelector(".zoom");
       zoomAttribute.classList.add("display-zoom");
+      this.count = 0;
     },
-    chevronNext() {},
+    chevronNext() {
+      let nbImg = this.getImages.length;
+
+      if (this.count < nbImg - 1) {
+        this.count++;
+      } else {
+        this.count = 0;
+      }
+    },
+    chevronPrevious() {
+      let nbImg = this.getImages.length;
+
+      if (this.count > 0) {
+        this.count--;
+      } else {
+        this.count = nbImg - 1;
+      }
+    },
+  },
+  computed: {
+    ...mapGetters(["getImages"]),
   },
 };
 </script>
@@ -50,11 +75,47 @@ export default {
   right: 0;
   margin-right: 5vw;
   color: rgba(255, 255, 255, 0.692);
-  font-size: clamp(16px, 4vw, 400px);
+  font-size: clamp(16px, 4vw, 90px);
+  font-weight: 400;
+  cursor: pointer;
+  z-index: 3;
+  &:hover {
+    color: white;
+    transform: scale(1.1);
+  }
+}
+.fa-chevron-left {
+  position: absolute;
+  left: 0;
+  margin-left: 5vw;
+  color: rgba(255, 255, 255, 0.692);
+  font-size: clamp(16px, 4vw, 90px);
+  font-weight: 400;
+  cursor: pointer;
+  z-index: 3;
+  &:hover {
+    color: white;
+    transform: scale(1.1);
+  }
+}
+.container-img {
+  position: relative;
+}
+img {
+  height: 100%;
+  width: 120vmin;
+  border-radius: 10px;
+}
+.fa-times {
+  position: absolute;
+  top: 0;
+  right: 0;
+  color: rgba(223, 223, 223, 0.966);
+  padding: 2vh;
+  font-size: clamp(16px, 2vw, 40px);
   font-weight: 400;
   cursor: pointer;
   &:hover {
-    color: white;
     transform: scale(1.1);
   }
 }
--- frontend/honfleur_project/src/store/index.js
+++ frontend/honfleur_project/src/store/index.js
@@ -2,6 +2,18 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    images: [
+      "1.jpg",
+      "2.jpg",
+      "3.jpg",
+      "4.jpg",
+      "5.jpg",
+      "6.jpg",
+      "7.jpg",
+    ]
+  },
+  getters: {
+    getImages: (state) => state.images,
   },
   mutations: {
   },
